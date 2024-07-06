aada22a3d67f849cd2a2975388d21541777f6822
NeemaMwende <gracepete12@gmail.com>
2024-07-06 18:33:35 +0300

    updates

--- blog-application/backend/index.js
+++ blog-application/backend/index.js
@@ -4,8 +4,9 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
 dotenv.config()
-
 const app = express();
+
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
