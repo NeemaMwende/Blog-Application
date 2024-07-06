4980d92dee9a8eb0bd46756e3a8c22f8fcc015b9
NeemaMwende <gracepete12@gmail.com>
2024-07-06 19:15:15 +0300

    updates

--- blog-application/backend/index.js
+++ blog-application/backend/index.js
@@ -7,6 +7,15 @@ dotenv.config()
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors({ origin: true, credentials: true}));
+
+mongoose.connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+        () => {
+            console.log("connected to db");
+        }
+);
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
