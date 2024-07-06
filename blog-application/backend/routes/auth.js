141c1323795aa72061586e8c673003cc69b8341b
NeemaMwende <gracepete12@gmail.com>
2024-07-06 20:54:23 +0300

    updates

---
+++ blog-application/backend/routes/auth.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bcrypt from 'bcrypt';
+import jwt from 'jwt';
+import User from '../models/User';
+import Post from '../models/Post';
+
+const router = express.Router();
+
+//Register
+router.post('/register', async (req, res) => {
+    const {username, email, password} = req.body;
+    try {
+        let user = await User.findOne({email})
+
+        if(user){
+            return res.status(400).json({message: 'User already exists'})
+        }
+
+        user = new User({username, email, password})
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        user.password = hashedPassword;
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
