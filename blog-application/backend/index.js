5097dd7187e8d6200024ca598e381a309f6675b0
NeemaMwende <gracepete12@gmail.com>
2024-07-06 20:33:42 +0300

    updates

--- blog-application/backend/index.js
+++ blog-application/backend/index.js
@@ -3,21 +3,25 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import cors from 'cors';
-dotenv.config()
+
+dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
-app.use(cors({ origin: true, credentials: true}));
+app.use(cors({ origin: true, credentials: true }));
 
 mongoose.connect(
     process.env.MONGO_URI,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
-        () => {
-            console.log("connected to db");
-        }
-);
+    // { useNewUrlParser: true, useUnifiedTopology: true }
+)
+.then(() => {
+    console.log("db connected");
+})
+.catch((error) => {
+    console.error("Error connecting to the database:", error);
+});
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log('server is running on port 5000')
+    console.log(`Server is running on port ${PORT}`);
 });
---
+++ blog-application/backend/models/Post.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const PostSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+export default mongoose.model('Post', PostSchema);
