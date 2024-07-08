b6116d014fe872c1d349d2c2977fe7b5d7dd3219
NeemaMwende <gracepete12@gmail.com>
2024-07-08 13:14:12 +0300

    updates

--- blog-application/backend/middleware/auth.js
+++ blog-application/backend/middleware/auth.js
@@ -6,4 +6,19 @@ const auth = (req, res, next) => {
     if(!authHeader){
         return res.status(401).json({error: 'No token provided'});
     }
+
+    const token = authHeader.split('')[1]
+
+    if(!token){
+        return res.status(401).json({error: 'No token provided, '})
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.userId = decoded.id;
+        return next();
+
+    } catch (error) {
+        return res.status(401).json({error: 'Token is not valid'});
+    }
 }
