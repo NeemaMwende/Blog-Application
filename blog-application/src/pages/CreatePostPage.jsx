192ea1dced22fdf1a7a97d7b28aff870e29814d6
NeemaMwende <gracepete12@gmail.com>
2024-07-09 22:24:04 +0300

    updates

--- blog-application/src/pages/CreatePostPage.jsx
+++ blog-application/src/pages/CreatePostPage.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { useState } from "react"
+import api from "../services/api"
 
 const CreatePostPage = () => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const token = localStorage.getItem('token');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const data = await api.createPost({title, content});
+      localStorage.setItem('token',data.token);
+      console.log(data);
+
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <div>
-      <h1>create post</h1>
+      <form onSubmit={handleSubmit}>
+
+            <div>
+              <label htmlFor="">Title</label>
+              <input type='email' value={title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+              
+              <div>
+                <label htmlFor="">Content</label>
+                <input type='password' value={content} onChange={(e) => setContent(e.target.value)} />
+              </div>
+
+              <button type='submit'> Create Post</button>
+
+      </form>
     </div>
   )
 }
