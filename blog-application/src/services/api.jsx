31bec37f8fe61271597e33d9f321556a52675978
NeemaMwende <gracepete12@gmail.com>
2024-07-09 21:14:47 +0300

    updates

--- blog-application/src/services/api.jsx
+++ blog-application/src/services/api.jsx
@@ -6,3 +6,27 @@ const register = async (userData) => {
     const response = await axios.post(`${API_URL}/auth/register`, userData);
     return response.data;
 }
+
+const login = async (userData) => {
+    const response = await axios.post(`${API_URL}/auth/list`, userData);
+    return response.data;
+}
+
+
+const createPost = async (postData, token) => {
+    const config = {
+        headers: {
+            'x-auth-token':token
+        }
+    }
+    const response = await axios.post(`${API_URL}/auth/posts`, postData,config);
+    return response.data;
+}
+
+
+const getPost = async (userData) => {
+    const response = await axios.post(`${API_URL}/auth/posts`, userData);
+    return response.data;
+}
+
+export default { register, login, getPost, createPost };
