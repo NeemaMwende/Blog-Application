1e8333f03090af712c3da227918277cb8d4c6f9d
NeemaMwende <gracepete12@gmail.com>
2024-07-09 19:17:51 +0300

    updates

--- blog-application/src/pages/RegisterPage.jsx
+++ blog-application/src/pages/RegisterPage.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const RegisterPage = () => {
+
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
-      <h1>Register</h1>
+      <form action="">
+        <input type='text' value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button type='submit'>Register</button>
+      </form>
     </div>
   )
 }
