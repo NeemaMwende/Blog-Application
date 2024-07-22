ddc7479a4f8be07fb612c927e4abed2a0e65e154
NeemaMwende <gracepete12@gmail.com>
2024-07-22 16:28:35 +0300

    updates

--- blog-application/backend/index.js
+++ blog-application/backend/index.js
@@ -12,8 +12,10 @@ import postRoute from './routes/posts.js';
 dotenv.config();
 const app = express();
 
+// app.use(bodyParser.json());
+// app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({ origin: 'YOUR_FRONTEND_RENDER_URL', credentials: true }));
 
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
